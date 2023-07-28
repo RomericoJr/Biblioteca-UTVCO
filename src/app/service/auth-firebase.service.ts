2f3a93c667ce8e25496f0b9628127f59ee355b39
Romérico Junior Orozco Vásquez <108032302+RomericoJr@users.noreply.github.com>
2023-07-27 22:01:22 -0600

    AUTH FIXED

--- src/app/service/auth-firebase.service.ts
+++ src/app/service/auth-firebase.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import firebase from 'firebase/compat';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthFirebaseService {
 
-  constructor() { }
+  constructor(
+    private auth: AngularFireAuth
+  ) { }
+
+
+  async registro(email:string, password:string){
+    try{
+      return await this.auth.createUserWithEmailAndPassword(email, password);
+      console.log('Usuario creado con exito');
+    } catch(error){
+      return error;
+    }
+  }
 }
