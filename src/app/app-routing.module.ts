d2d08589bf847a019df28bf48c2b63b2f52e62e8
JorgeMateos13 <mateosjorge026@gmail.com>
2023-08-14 20:37:05 -0600

    cambio1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   { path: 'book', loadChildren: () => import('./components/book/book.module').then(m => m.BookModule)},
   { path: 'estadias', loadChildren: () => import('./components/estadias/estadias.module').then(m => m.EstadiasModule)},
   { path: 'header', loadChildren: () => import('./components/header/header.module').then(m => m.HeaderModule)},
-
+  //{ path: 'login', component: LoginComponent, ...,canActivate(()=>) },
   { path: '', redirectTo: 'auth', pathMatch: 'full' },
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { environment } from 'src/environments/environment.development';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { provideAuth,getAuth } from '@angular/fire/auth';
+import { BookListComponent } from './components/book/book-list/book-list.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
