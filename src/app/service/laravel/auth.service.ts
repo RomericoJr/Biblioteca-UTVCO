991fce4e28fefad7a94c4e26fc318d68106ea9ca
Romérico Junior Orozco Vásquez <108032302+RomericoJr@users.noreply.github.com>
2023-12-07 21:18:39 -0600

    fixed rutas and guard

--- src/app/service/laravel/auth.service.ts
+++ src/app/service/laravel/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { login } from 'src/app/interface/auth';
+import { ToolsService } from '../tools.service';
 
 // API path
 const API_URL=  'http://localhost:8000/api/Back%%Biblioteca_UTVCO/';
@@ -12,7 +13,8 @@ const API_URL=  'http://localhost:8000/api/Back%%Biblioteca_UTVCO/';
 
 export class AuthService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private toolS: ToolsService
   ) { }
 
 
@@ -39,4 +41,28 @@ export class AuthService {
     return this.http.get<any>(`${API_URL}user-profile`, {headers: headers});
   }
 
+  private isLoggedIn = new BehaviorSubject<boolean>(false);
+  toggleLogin(state: boolean): void {
+    this.isLoggedIn.next(state);
+  }
+
+  status() {
+    const localData: any = localStorage.getItem('token');
+    if (!localData) {
+      this.isLoggedIn.next(false);
+      // console.log('no hay token');
+    } else {
+      const expirationDate = this.toolS.getTokenExpiration();
+      const URL = new Date(expirationDate ? expirationDate : '');
+      const currentDate = new Date();
+      if (URL < currentDate) {
+        this.isLoggedIn.next(false);
+        // console.log('token expirado');
+      } else {
+        this.isLoggedIn.next(true);
+        // console.log('token valido');
+      }
+    }
+    return this.isLoggedIn.asObservable();
+  }
 }
